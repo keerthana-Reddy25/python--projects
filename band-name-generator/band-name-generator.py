d63a856129eebe046bcea7d0ff12f0653409ef31
= <=>
2023-12-20 15:39:23 -0500

    two  projects added //

---
+++ band-name-generator/band-name-generator.py
@@ -0,0 +1,6 @@
+print("Welcome to the Band Name Generator")
+print("In order to generate you band name I need few details \n ")
+cityName = input("Please tell me the name of the city you grew up : ")
+petName = input("Please tell me your pet name : ")
+bandName = cityName+ " " +petName
+print("So, Your Band Name is ",bandName)
